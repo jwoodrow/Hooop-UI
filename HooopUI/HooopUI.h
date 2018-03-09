bf2df07045fe1eadaeddcc0331f8c388e966f4b4
jwoodrow <jwoodrow@student.42.fr>
2018-03-09 06:39:40 +0100

    Initial Commit

---
+++ HooopUI/HooopUI.h
@@ -0,0 +1,19 @@
+//
+//  HooopUI.h
+//  HooopUI
+//
+//  Created by James Woodrow on 09/03/2018.
+//  Copyright © 2018 Hooop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HooopUI.
+FOUNDATION_EXPORT double HooopUIVersionNumber;
+
+//! Project version string for HooopUI.
+FOUNDATION_EXPORT const unsigned char HooopUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HooopUI/PublicHeader.h>
+
+
